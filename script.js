bed04cd7db0cc8463fd60dc986d00544e595cedc
nathan11883 <nathandyao@gmail.com>
2016-09-23 19:46:22 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,28 +1,73 @@
 <html>
   <head>
-    <script src="https://code.jquery.com/jquery-3.1.0.min.js"></script>
-    <link href="style.css" rel="stylesheet" />
-  </head>
-  <body>
-    <div id="bot-name">
-      Bot Name Here
-    </div>
-    <div id="bot-input-container">        
-      <input id="bot-input" type="text"/>      
-    </div>
-    <div>
-      <div id="bot-output"> 
+  <script src="https://code.jquery.com/jquery-3.1.0.min.js"></script>
 
-      </div>      
-    </div>
-    <img id="robot-pic" src="https://pbs.twimg.com/profile_images/484689868140711936/WQ6vjWbW_400x400.jpeg">
-    <script src="script.js">
-    </script>
-  </body>
-</html>
-    else if (text ==="move") {
-      gameOutput.animate({top: 500});
-    }
+
+
+
+
+    <style>
+    #bot-name {
+  color: red;
 }
 
+#bot-output {
+position: relative;
+top: 200px;        
+}
 
+#robot-pic {
+width: 50px;
+}
+  </style>
+
+
+
+
+
+    </head>
+    <body>
+    <div id="bot-name">
+      Bot Name Here
+        </div>
+        <div id="bot-input-container">        
+          <input id="bot-input" type="text"/>      
+            </div>
+            <div>
+            <div id="bot-output"> 
+
+              </div>      
+              </div>
+
+              <img id="bot-output" src="http://img5.imgtn.bdimg.com/it/u=3409396837,1556064680&fm=21&gp=0.jpg">
+
+
+
+
+
+
+
+
+                <script>
+
+
+                var gameInput = $("#bot-input");
+var gameOutput  = $("#bot-output");
+gameInput.keydown(function(keydownEvent) { 
+  // the key code for enter is 13
+  if (keydownEvent.keyCode == 13) {      
+    parseText(gameInput.val()); 
+    gameInput.val("");   
+  }
+}
+
+                  function parseText(text) {      
+  if (text === "Good bye") {
+    gameOutput.html("Bye!");
+  } else if (text==="name?") {
+    gameOutput.html("All powerful pizza Bot")
+  } else if (text === "robot come out!") {
+    gameOutput.html("🤖️🤖️")
+  } else if (text==="move"){
+    gameOutput.animate({top:500}); 
+  }
